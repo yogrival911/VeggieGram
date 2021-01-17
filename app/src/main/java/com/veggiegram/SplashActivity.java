e2219c6832e2600d455cdad36db828dbe9cc2817
YOGESH <yogrival911@gmail.com>
2021-01-17 12:50:12 +0530

    VeggieGram

---
+++ app/src/main/java/com/veggiegram/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.veggiegram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+ImageView imageViewSplash;
+private static int SPLASH_SCREEN_TIME_OUT=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+        imageViewSplash = findViewById(R.id.imageViewSplash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+
+        }, SPLASH_SCREEN_TIME_OUT);
+
+
+    }
+}
