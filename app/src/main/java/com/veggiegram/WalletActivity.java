0eae5c63768ba99df5dff291d46fb92117b963b7
YOGESH <yogrival911@gmail.com>
2021-02-15 13:04:19 +0530

    VeggieGram

--- app/src/main/java/com/veggiegram/WalletActivity.java
+++ app/src/main/java/com/veggiegram/WalletActivity.java
@@ -1,14 +1,25 @@
 package com.veggiegram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class WalletActivity extends AppCompatActivity {
-
+Toolbar toolbarWallet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
+
+        toolbarWallet = findViewById(R.id.toolbarWallet);
+        setSupportActionBar(toolbarWallet);
+        getSupportActionBar().setTitle("Wallet");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
