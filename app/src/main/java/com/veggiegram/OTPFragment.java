512229f7f84aced768d7bff38c8793bc17598b36
YOGESH <yogrival911@gmail.com>
2021-02-19 18:19:36 +0530

    VeggieGram

--- app/src/main/java/com/veggiegram/OTPFragment.java
+++ app/src/main/java/com/veggiegram/OTPFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.os.CountDownTimer;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.veggiegram.R;
@@ -20,7 +22,11 @@ import com.veggiegram.responses.otp.OTPResponse;
 import com.veggiegram.responses.otp.SendOTPObject;
 import com.veggiegram.retrofit.RetrofitClientInstance;
 import com.veggiegram.retrofit.RetrofitIInterface;
+import com.veggiegram.ui.favourite.FavouriteFragmentDirections;
 import com.veggiegram.ui.register.SigninFragment;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -33,6 +39,7 @@ EditText etOTP;
 Button verify;
 OTPResponse otpResponse = new OTPResponse();
 SharedPreferences sharedPreferences;
+TextView resendCounter,enterWrong;
     public OTPFragment() {
 
     }
@@ -43,17 +50,46 @@ SharedPreferences sharedPreferences;
 
         verify = view.findViewById(R.id.verify);
         etOTP = view.findViewById(R.id.etOTP);
+        resendCounter = view.findViewById(R.id.resendCounter);
+        enterWrong = view.findViewById(R.id.enterWrong);
 
         String mobile_passed = OTPFragmentArgs.fromBundle(getArguments()).getMobileNo();
         String user_id = OTPFragmentArgs.fromBundle(getArguments()).getUserId();
         String name = OTPFragmentArgs.fromBundle(getArguments()).getName();
         String email = OTPFragmentArgs.fromBundle(getArguments()).getEmail();
 
+        NavHostFragment navHostFragment =(NavHostFragment)getActivity().getSupportFragmentManager().findFragmentById(R.id.nav_host);
 
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
 
         Retrofit retrofit = RetrofitClientInstance.getInstance();
         RetrofitIInterface retrofitIInterface = retrofit.create(RetrofitIInterface.class);
+
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+                // Used for formatting digit to be in 2 digits only
+
+                NumberFormat f = new DecimalFormat("00");
+
+                long min = (millisUntilFinished / 60000) % 60;
+                long sec = (millisUntilFinished / 1000) % 60;
+
+                resendCounter.setText("Resend in "+f.format(min) + ":" + f.format(sec));
+            }
+
+            public void onFinish() {
+                resendCounter.setText("00:00");
+            }
+        }.start();
+
+        enterWrong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navHostFragment.getNavController().navigate(OTPFragmentDirections.actionOTPFragmentToSigninFragment());
+            }
+        });
 
         retrofitIInterface.sendOtp(new SendOTPObject(mobile_passed,"")).enqueue(new Callback<OTPResponse>() {
             @Override
